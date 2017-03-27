f7d732812904304556e3ae71183ba4e291ebaaea
Monzyy <magnushansen12@hotmail.com>
2017-03-27 15:29:14 +0200

    adds initial brsaver.py and Video.py

---
+++ Video.py
@@ -0,0 +1,15 @@
+import re
+
+class Video:
+    name = ""
+    path = ""
+    format = ""
+    bytes = 0
+    seconds = 0
+    bitrate_kbps = 0
+
+    def __init__(self, path):
+        self.path = path
+        tmp = path.split('/')[-1]
+        self.name = tmp.split('.')[0]
+        self.format = tmp.split('.')[1]
