8de1342a416e231e98624d36585f9ffa21747318
Jonas Kloster Jacobsen <joklost@gmail.com>
2017-03-28 10:17:09 +0200

    Moves main part of script to main function

--- brsaver.py
+++ brsaver.py
@@ -16,38 +16,44 @@ parser.add_argument('-b', '--bitrate', dest='bitrate',
                     help='bitrate to convert video files to',
                     type=int, default=8192)
 args = parser.parse_args()
-bitrate_target = args.bitrate
-max_bitrate = bitrate_target + 1000
-path = args.path
-dirs = os.listdir(path)
 
-# Get all videofiles with proper filetypes from the folder and all subfolders
-vfiles = [os.path.join(root, name)
-          for root, dirs, files in os.walk(path)
-          for name in files
-          if name.endswith((".mkv", ".mp4"))]
 
-videos = []
+def main(args):
+    bitrate_target = args.bitrate
+    max_bitrate = bitrate_target + 1000
+    path = args.path
 
-# Create video objects from the videofile names
-for vfile in vfiles:
-    videos.append(Video(vfile))
+    # Get all videofiles with proper filetypes from the folder and all subfolders
+    vfiles = [os.path.join(root, name)
+              for root, dirs, files in os.walk(path)
+              for name in files
+              if name.endswith((".mkv", ".mp4"))]
 
-fileIndex = 0
+    videos = []
 
-# Run ffmpeg -i [videofile] to get bitrates
-for file in vfiles:
-    tmp = subprocess.run(["ffmpeg", "-i", file], stderr=subprocess.PIPE)
-    lines = tmp.stderr.splitlines()
-    for line in lines:
-        li = line.decode("utf-8")
-        if "bitrate" in li:
-            videos[fileIndex].bitrate_kbps = int(re.findall(r"[\d+']+", li)[-1])
-            fileIndex += 1
+    # Create video objects from the videofile names
+    for vfile in vfiles:
+        videos.append(Video(vfile))
 
-# Create lower bitrate videos, if the videos bitrate is higher than max_bitrate
-for video in videos:
-    if video.bitrate_kbps > max_bitrate and video.bitrate_kbps != 0:
-        subprocess.run(["ffmpeg", "-y", "-i", video.path, "-b:v",
-                        str(bitrate_target) + "k",
-                        path + "new" + video.name + "." + video.format])
+    fileIndex = 0
+
+    # Run ffmpeg -i [videofile] to get bitrates
+    for file in vfiles:
+        tmp = subprocess.run(["ffmpeg", "-i", file], stderr=subprocess.PIPE)
+        lines = tmp.stderr.splitlines()
+        for line in lines:
+            li = line.decode("utf-8")
+            if "bitrate" in li:
+                videos[fileIndex].bitrate_kbps = int(re.findall(r"[\d+']+", li)[-1])
+                fileIndex += 1
+
+    # Create lower bitrate videos, if the videos bitrate is higher than max_bitrate
+    for video in videos:
+        if video.bitrate_kbps > max_bitrate and video.bitrate_kbps != 0:
+            subprocess.run(["ffmpeg", "-y", "-i", video.path, "-b:v",
+                            str(bitrate_target) + "k",
+                            path + "new" + video.name + "." + video.format])
+
+
+if __name__ == '__main__':
+    main(args)
