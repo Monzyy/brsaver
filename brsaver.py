8b286d9dd544f5cd30402524bee5b27ca13b0fe4
Jonas Kloster Jacobsen <joklost@gmail.com>
2017-03-28 16:12:37 +0200

    #5: Adds the option to add multiple directories via the cmd argument

--- brsaver.py
+++ brsaver.py
@@ -4,11 +4,10 @@ import os
 import re
 import subprocess
 
-
 parser = argparse.ArgumentParser(description=('Convert .mkv and .mp4 files '
                                               'to a lower bitrate to save disk space.'))
-parser.add_argument('-d', '--directory', dest='path',
-                    metavar='PATH', action='store',
+parser.add_argument('-d', '--directory', dest='paths',
+                    metavar='PATH', action='store', nargs='*',
                     help='path to folder containing video files',
                     type=str, required=True)
 parser.add_argument('-b', '--bitrate', dest='bitrate',
@@ -19,40 +18,40 @@ args = parser.parse_args()
 
 
 def main(args):
-    bitrate_target = args.bitrate
-    max_bitrate = bitrate_target + 1000
-    path = args.path
+    for path in args.paths:
+        bitrate_target = args.bitrate
+        max_bitrate = bitrate_target + 1000
 
-    # Get all videofiles with proper filetypes from the folder and all subfolders
-    vfiles = [os.path.join(root, name)
-              for root, dirs, files in os.walk(path)
-              for name in files
-              if name.endswith((".mkv", ".mp4"))]
+        # Get all videofiles with proper filetypes from the folder and all subfolders
+        vfiles = [os.path.join(root, name)
+                  for root, dirs, files in os.walk(path)
+                  for name in files
+                  if name.endswith((".mkv", ".mp4"))]
 
-    videos = []
+        videos = []
 
-    # Create video objects from the videofile names
-    for vfile in vfiles:
-        videos.append(Video(vfile))
+        # Create video objects from the videofile names
+        for vfile in vfiles:
+            videos.append(Video(vfile))
 
-    fileIndex = 0
+        fileIndex = 0
 
-    # Run ffmpeg -i [videofile] to get bitrates
-    for file in vfiles:
-        tmp = subprocess.run(["ffmpeg", "-i", file], stderr=subprocess.PIPE)
-        lines = tmp.stderr.splitlines()
-        for line in lines:
-            li = line.decode("utf-8")
-            if "bitrate" in li:
-                videos[fileIndex].bitrate_kbps = int(re.findall(r"[\d+']+", li)[-1])
-        fileIndex += 1
+        # Run ffmpeg -i [videofile] to get bitrates
+        for file in vfiles:
+            tmp = subprocess.run(["ffmpeg", "-i", file], stderr=subprocess.PIPE)
+            lines = tmp.stderr.splitlines()
+            for line in lines:
+                li = line.decode("utf-8")
+                if "bitrate" in li:
+                    videos[fileIndex].bitrate_kbps = int(re.findall(r"[\d+']+", li)[-1])
+            fileIndex += 1
 
-    # Create lower bitrate videos, if the videos bitrate is higher than max_bitrate
-    for video in videos:
-        if video.bitrate_kbps > max_bitrate and video.bitrate_kbps != 0:
-            subprocess.run(["ffmpeg", "-y", "-i", video.path, "-b:v",
-                            str(bitrate_target) + "k",
-                            path + "new" + video.name + "." + video.format])
+        # Create lower bitrate videos, if the videos bitrate is higher than max_bitrate
+        for video in videos:
+            if video.bitrate_kbps > max_bitrate and video.bitrate_kbps != 0:
+                subprocess.run(["ffmpeg", "-y", "-i", video.path, "-b:v",
+                                str(bitrate_target) + "k",
+                                path + "new" + video.name + "." + video.format])
 
 
 if __name__ == '__main__':
