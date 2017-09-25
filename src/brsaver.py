bed1629520281ea2a1789dadc6ab2a5f559ec9b5
Magnus <magnushansen12@hotmail.com>
2017-09-25 19:59:21 +0200

    copy all audio and subtitle tracks #11

--- src/brsaver.py
+++ src/brsaver.py
@@ -37,37 +37,49 @@ def collect_video_files(paths):
     return files
 
 
+def intersperse(lst, item):
+    result = [item] * (len(lst) * 2)
+    result[1::2] = lst
+    return result
+
+
 def main(args):
     check_python_version()
     check_ffmpeg_version()
     videocodec = check_videocodec()
 
     bitrate_target = args.bitrate
-    max_bitrate = bitrate_target + 1000
+    max_bitrate = bitrate_target + 1024
 
     videos = []
     video_files = collect_video_files(args.paths)
-    # Create video objects from the videofile names
+    # Create video objects from the video file names
     for video_file in video_files:
         videos.append(Video(video_file))
 
     # Run ffmpeg -i [videofile] to get bitrates
     fileIndex = 0
-    for file in video_files:
-        out = subprocess.run(["ffmpeg", "-i", file], stderr=subprocess.PIPE)
+    for video in videos:
+        out = subprocess.run(["ffmpeg", "-i", video.fullpath], stderr=subprocess.PIPE)
         lines = out.stderr.splitlines()
         for line in lines:
             li = line.decode("utf-8")
             if "bitrate" in li:
-                videos[fileIndex].bitrate_kbps = int(re.findall(r"[\d+']+", li)[-1])
+                video.bitrate_kbps = int(re.findall(r"[\d+']+", li)[-1])
+
+            if "Stream" in li:
+                video.streams.append(li[12:-len(li)+15])
         fileIndex += 1
 
     # Create lower bitrate videos, if the videos bitrate is higher than max_bitrate
     for video in videos:
         if video.bitrate_kbps > max_bitrate:
-            subprocess.run(["ffmpeg", "-y", "-i", video.fullpath, "-c:v", videocodec, "-b:v",
-                            str(bitrate_target) + "k",
-                            video.directory + "/new" + video.name + video.format])
+            streams = intersperse(video.streams, "-map")
+            command = ["ffmpeg", "-y", "-i", video.fullpath, "-c:v", videocodec, "-b:v", str(bitrate_target) + "k",
+                       "-c:a", "copy", "-c:s", "copy"]
+            command.extend(streams)
+            command.extend([video.directory + "/new" + video.name + video.format])
+            subprocess.run(command)
 
 
 if __name__ == "__main__":
